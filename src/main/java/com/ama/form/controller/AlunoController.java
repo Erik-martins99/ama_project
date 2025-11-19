3e67febf74a479edc3555b4810d624ba29c8ccb5
ErikPC <erik.oliv.martins@gmail.com>
2025-11-19 18:56:09 -0300

    Add swagger doc

--- src/main/java/com/ama/form/controller/AlunoController.java
+++ src/main/java/com/ama/form/controller/AlunoController.java
@@ -17,24 +17,53 @@ import com.ama.form.dto.Aluno.CreateAlunoDTO;
 import com.ama.form.model.Aluno;
 import com.ama.form.service.AlunoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/aluno")
+@Tag(name = "Gerenciamento de alunos", description = "APIs para o cadastro de novos alunos no AMA")
 public class AlunoController {
 
     @Autowired
     private AlunoService alunoService;
 
+    @Operation(summary = "Criar Aluno", description = "Registra um novo aluno no sistema com base no formulario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Student created with successfully",
+                    content = @Content(schema = @Schema(implementation = CreateAlunoDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request data",
+                    content = @Content(schema = @Schema()))
+    })
     @PostMapping
     public ResponseEntity<Aluno> criar(@RequestBody CreateAlunoDTO dto) {
         Aluno aluno = alunoService.criarAluno(dto);
         return ResponseEntity.ok(aluno);
     }
 
+    @Operation(summary = "Alunos", description = "Retorna todos os alunos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Students find successfully",
+                    content = @Content(schema = @Schema(implementation = CreateAlunoDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request data",
+                    content = @Content(schema = @Schema()))
+    })
     @GetMapping
     public ResponseEntity<List<Aluno>> listarTodos() {
         return ResponseEntity.ok(alunoService.findAll());
     }
 
+    @Operation(summary = "Aluno", description = "Retorna um aluno pelo seu ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Student find successfully",
+                    content = @Content(schema = @Schema(implementation = CreateAlunoDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Student not found",
+                    content = @Content(schema = @Schema()))
+    })
     @GetMapping("/{id}")
     public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
         return alunoService.findById(id)
@@ -42,6 +71,13 @@ public class AlunoController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Atualizar Aluno", description = "Atualiza os dados de um aluno pelo seu ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Student updeted with successfully",
+                    content = @Content(schema = @Schema(implementation = CreateAlunoDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Student not found",
+                    content = @Content(schema = @Schema()))
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Aluno> atualizar(@PathVariable Long id,
                                            @RequestBody CreateAlunoDTO dto) {
@@ -50,6 +86,12 @@ public class AlunoController {
         return ResponseEntity.ok(atualizado);
     }
 
+    @Operation(summary = "Deleta Aluno", description = "Deleta um aluno pelo seu ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Student deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Student not found",
+                    content = @Content(schema = @Schema()))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deletar(@PathVariable Long id) {
         alunoService.delete(id);
